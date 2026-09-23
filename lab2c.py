d987749a6c232db5334b9c618cafce88c13d07de
JudeFlores <jflores31@myseneca.ca>
2026-09-23 18:53:21 +0000

    Completed lab 2c

--- lab2c.py
+++ lab2c.py
@@ -2,21 +2,18 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author:Jude Flores
+# Date:September 23, 2026
 # Purpose: Practice using if, elif, and else statments.
 # Usage: ./lab2c.py
 
-# TO DO 1:
-# Prmopt the user to enter a sentence, save it in the variable str1
-# Prmopt the user to enter another sentence, save it in the variable str2
-#
-# Use if, elif, and else statments with the len() function to check which of the 2 is longer.
-# The final result should be:
-# ---- is longer then ----
-# If they are equal then print:
-# ---- and ---- are equal.
-# Get input from the user
+str1 = input("Enter a sentence: ")
+str2 = input("Enter another sentence: ")
 
-
+if len(str1) > len(str2):
+    print("str1 is longer than str2")
+elif len(str1) == len(str2):
+    print("str1 and str2 are equal.")
+else:
+    print("str2 is longer than str1")
 
