fa1f35860f892eb001718309d6050601ad64f016
JudeFlores <jflores31@myseneca.ca>
2026-09-23 18:35:54 +0000

    Completed lab 1d

--- lab2a.py
+++ lab2a.py
@@ -1,13 +1,18 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author:Jude Flores
+# Date: September 23, 2026
 # Purpose: Create a variable, check its type and print the variable.
 # Usage: ./lab2a.py
 
 # TO DO 1: Follow the instructions given in README.md file
-
-
+x = input("Enter a number: ")
+print(type(x))
+x = int(x)
+if x > 6:
+    print("x is greater than 6!")
+if (x >= 4) and (x < 12):
+    print("x is greater/equal to 5 and less than 12!")
 
 
