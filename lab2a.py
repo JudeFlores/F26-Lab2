493c26e0022ce7688caf94a21bcd7230a2c77c28
JudeFlores <jflores31@myseneca.ca>
2026-09-23 18:46:58 +0000

    Completed lab 2a

--- lab2a.py
+++ lab2a.py
@@ -13,6 +13,6 @@ x = int(x)
 if x > 6:
     print("x is greater than 6!")
 if (x >= 4) and (x < 12):
-    print("x is greater/equal to 5 and less than 12!")
+    print("x is greater/equal to 5 and less than 12!") 
 
 
