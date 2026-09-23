db42d24e19780397367b24410bd3f9168c944691
JudeFlores <jflores31@myseneca.ca>
2026-09-23 19:16:33 +0000

    Completed Lab 2e

--- lab2e.py
+++ lab2e.py
@@ -1,9 +1,23 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Jude Flores
+# Date: September 23, 2026
 # Purpose: Learn how to use command line arguments.
 # Usage: ./lab2e.py
 
 # TO DO 1: Follow the instructions given in README.md file
+import sys
+
+print(sys.argv)
+print(len(sys.argv))
+num_args = len(sys.argv) - 1
+if num_args < 2:
+    print("This script requires exactly two arguments. No arguments were provided!")
+else:
+    name = sys.argv[1]
+    age = sys.argv[2]
+    if num_args == 2:
+        print(f"Hi {name}, you are {age} years old and the script recieved 2 arguments!")
+    else:
+        print(f"Hi {name}, you are {age} years old and this script has received {num_args} arguments!")
