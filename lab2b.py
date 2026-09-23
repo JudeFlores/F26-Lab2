5d71ecc403a799e6478b11d229f024317497d999
JudeFlores <jflores31@myseneca.ca>
2026-09-23 18:46:13 +0000

    Completed lab 2b

--- lab2b.py
+++ lab2b.py
@@ -1,11 +1,16 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author:Jude Flores
+# Date: September 23, 2026
 # Purpose: Practice using if and else statments.
 # Usage: ./lab2b.py
 
 # TO DO 1:
 # Follow the instructions given in the README.md file.
-
+num = input("Enter a four digit number: ")
+num = int(num)
+if num == 1984:
+    print("George Orwell")
+else:
+    print("Not quite right!")
