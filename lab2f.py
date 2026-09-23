1bf03509ee5acd28c18a9622ef6f9ec55a646be3
JudeFlores <jflores31@myseneca.ca>
2026-09-23 19:39:16 +0000

    Completed lab 2f

--- lab2f.py
+++ lab2f.py
@@ -7,3 +7,15 @@
 # Usage: ./lab2f.py
 
 # TO DO 1: Follow the instructions given in README.md file
+import sys
+
+num_args = len(sys.argv)-1
+if num_args < 2:
+    print("This script requires at least two arguments")
+else:
+    name = sys.argv[1]
+    age = sys.argv[2]
+    if num_args == 2:
+        print(f"Hi {name}, you are {age} years old and the script received {num_args} arguments")
+    else:
+        print(f"Hi {name}, you are {age} and the script has recieved {num_args} arguments.")
